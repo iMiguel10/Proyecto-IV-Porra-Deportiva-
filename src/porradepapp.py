b3983f32600e95ef109fba26139c5ed54bfcd099
iMiguel10 <imiguel10@hotmail.es>
2018-12-12 12:45:40 +0100

    Añadir funciones a la api close #37

--- src/porradepapp.py
+++ src/porradepapp.py
@@ -38,11 +38,43 @@ def partido(jornada,n):
 
 	return jsonify(partido=datos)
 
-@app.route('/apuesta/<usuario>', methods = ['GET'])
+# Si usas el método delete borra la última apuesta del usuario
+# Método post añade una apuesta a un usuario o añade un usuario con esa apuesta
+@app.route('/apuesta/<usuario>', methods = ['GET','DELETE','POST'])
 def apuesta(usuario):
 	p=funcionesbasicasDB.Apuestas()
-	datos=p.getApuestas(usuario)
+	datos = []
+	if request.method == 'DELETE':
+		datos=p.delApuesta(usuario)
+	if request.method == 'GET':
+		datos=p.getApuestas(usuario)
+	if request.method == 'POST':
+		json_data = request.get_json()
+		partido = json_data['partido']
+		resultado = json_data['resultado']
+		datos=p.setApuestas(usuario,partido,resultado)
+	
 	return jsonify(apuestas=datos)
+
+# Función que te muestra todos los apostantes
+@app.route('/apostantes', methods = ['GET'])
+def apostantes():
+	p=funcionesbasicasDB.Apuestas()
+	datos = []
+	if request.method == 'GET':
+		datos=p.getApostantes()
+	
+	return jsonify(apostantes=datos)
+
+# Función que elimina un apostante
+@app.route('/apostante/<usuario>', methods = ['DELETE'])
+def delApostante(usuario):
+	p=funcionesbasicasDB.Apuestas()
+	datos = []
+	if request.method == 'DELETE':
+		datos=p.delApostante(usuario)
+	
+	return jsonify(apostante=datos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
